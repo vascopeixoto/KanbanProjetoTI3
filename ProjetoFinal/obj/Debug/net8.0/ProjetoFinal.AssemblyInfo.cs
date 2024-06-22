f9f6a3c7c03afcc5f7a54356c6355f8791882eaf
vascopeixoto <vascomfpeixoto@gmail.com>
2024-06-22 15:32:50 +0100

    change navbar if user not logged

--- ProjetoFinal/obj/Debug/net8.0/ProjetoFinal.AssemblyInfo.cs
+++ ProjetoFinal/obj/Debug/net8.0/ProjetoFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+658c282fbfd6c19848a26a48185bb8c299bc4dbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c72e5473aad197eec5ac65b50cdc109e56d4c9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
