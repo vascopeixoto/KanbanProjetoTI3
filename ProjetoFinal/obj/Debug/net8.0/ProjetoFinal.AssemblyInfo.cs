131bc352199b5ef644640f7fccd4b4e541fee1cb
vascopeixoto <vascomfpeixoto@gmail.com>
2024-06-22 16:00:44 +0100

    Little visual improvements

--- ProjetoFinal/obj/Debug/net8.0/ProjetoFinal.AssemblyInfo.cs
+++ ProjetoFinal/obj/Debug/net8.0/ProjetoFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c72e5473aad197eec5ac65b50cdc109e56d4c9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f6a3c7c03afcc5f7a54356c6355f8791882eaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
